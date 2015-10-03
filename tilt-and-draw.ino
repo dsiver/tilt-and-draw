cc925a0cc1ee24643e9b716bb95d91190ab54de8
David Siver <dsiver1@my.westga.edu>
2015-10-03 19:27:21 -0400

    draws continuous line

--- tilt-and-draw.ino
+++ tilt-and-draw.ino
@@ -13,9 +13,17 @@
 
 #define DEBUG 0
 #define serialBaudRate 9600
+#define xMin 0
+#define xMax EsploraTFT.width() - 1
+#define yMin 0
+#define yMax EsploraTFT.height() - 1
+#define low -100
+#define high 100
+#define sensitivity 20
+#define pixels 1
 
-int xPos = EsploraTFT.width()/2;
-int yPos = EsploraTFT.height()/2;
+int xPos = EsploraTFT.width() / 2;
+int yPos = EsploraTFT.height() / 2;
 
 void setup() {
   Serial.begin(serialBaudRate);
@@ -26,4 +34,24 @@ void setup() {
 void loop() {
   int xAxis = Esplora.readAccelerometer(X_AXIS);
   int yAxis = Esplora.readAccelerometer(Y_AXIS);
+  setPosition(xAxis, yAxis);
+  EsploraTFT.stroke(255, 255, 255);
+  EsploraTFT.point(xPos, yPos);
+  delay(100);
 }
+
+void setPosition(int xAxis, int yAxis){
+  if (xAxis < sensitivity && xAxis > -sensitivity) {
+    xPos = xPos;
+  }
+  else {
+    xPos += map(xAxis, low, high, pixels, -pixels);
+  }
+  if (yAxis < sensitivity && yAxis > -sensitivity) {
+    yPos = yPos;
+  }
+  else {
+    yPos += map(yAxis, low, high, -pixels, pixels);
+  }
+}
+
