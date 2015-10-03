e93aa94bb90ed8210b38cb0e46a529447a75ce61
David Siver <dsiver1@my.westga.edu>
2015-10-03 17:19:20 -0400

    set start position, reading accelerometer x,y in loop

--- tilt-and-draw.ino
+++ tilt-and-draw.ino
@@ -11,8 +11,19 @@
 #include <SPI.h>
 #include <TFT.h>
 
+#define DEBUG 0
+#define serialBaudRate 9600
+
+int xPos = EsploraTFT.width()/2;
+int yPos = EsploraTFT.height()/2;
+
 void setup() {
+  Serial.begin(serialBaudRate);
+  EsploraTFT.begin();
+  EsploraTFT.background(0, 0, 0);
 }
 
 void loop() {
+  int xAxis = Esplora.readAccelerometer(X_AXIS);
+  int yAxis = Esplora.readAccelerometer(Y_AXIS);
 }
